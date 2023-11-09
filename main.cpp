1a5738ff22d8ecf5aa0b5ab2c06e8a1d5f1c86a5
WFERRIE <ferriewi@gmail.com>
2023-11-09 17:46:27 -0500

    added fisher yates shuffle function to shuffle a matrix row-wise

--- main.cpp
+++ main.cpp
@@ -2,6 +2,23 @@
 #include "csvreader.h"
 #include <iostream>
 
+
+nc::NdArray<double> fisher_yates_shuffle(nc::NdArray<double> input, const int& n_samples) {
+
+    // shuffle an array, row-wise
+
+    for (int i = n_samples - 1; i > 0; i--) {
+        int j = std::rand() % (i + 1);
+
+        nc::NdArray<double> temp = input(j, input.cSlice());
+        input.put(j, input.cSlice(), input(i, input.cSlice())); 
+        input.put(i, input.cSlice(), temp); 
+
+    }
+
+    return input;
+}
+
 int main() {
 
     std::vector<std::vector<double>> data = readCSV("iris.csv");
@@ -17,6 +34,12 @@ int main() {
         }
     }
 
+    // std::cout << matrix({0, n_samples}, {0, n_features}) << std::endl;
+
+    matrix = fisher_yates_shuffle(matrix, n_samples);
+
     std::cout << matrix << std::endl;
+
     return 0;
+
 }
