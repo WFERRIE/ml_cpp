fb170fca077b63cce570196bcf75d004f439b3a9
WFERRIE <ferriewi@gmail.com>
2023-11-20 17:45:56 -0500

    added WIP logistic regression code. gradient explodes so isn't working yet

--- main.cpp
+++ main.cpp
@@ -19,12 +19,25 @@ nc::NdArray<double> fisher_yates_shuffle(nc::NdArray<double> input, const int& n
     return input;
 }
 
+nc::NdArray<double> sigmoid(nc::NdArray<double> z, const nc::uint32 n_samples) {
+    auto ones = nc::ones<double>(n_samples, 1);
+    return ones / (ones + nc::exp(-z));
+}
+
+double compute_BCE_cost(nc::NdArray<double> predictions, nc::NdArray<double> y, const nc::uint32 n_samples) {
+
+    auto cost = -(1.0 / 150.0) * nc::sum<double>(y * nc::log(predictions) + (1.0 - y) * nc::log(1.0 - predictions)); // cross entropy loss
+
+    return cost(0, 0); // return it as a double instead of a 1 element nc::NdArray
+
+}
+
 int main() {
 
     std::vector<std::vector<double>> data = readCSV("iris.csv");
 
     const nc::uint32 n_samples = 150;
-    const nc::uint32 n_features = 5;
+    const nc::uint32 n_features = 5; // this counts the labels as a feature, so n_features-1 input features, 1 output feature
 
     auto matrix = nc::NdArray<double>(n_samples, n_features);
 
@@ -38,7 +51,48 @@ int main() {
 
     matrix = fisher_yates_shuffle(matrix, n_samples);
 
-    std::cout << matrix << std::endl;
+
+    auto y = matrix(matrix.rSlice(), n_features - 1);
+    auto X = matrix(matrix.rSlice(), {0, n_features - 1});
+
+
+    int n_iters = 100;
+
+    nc::NdArray<double> weights = nc::zeros<double>(n_features - 1, 1);
+    double bias = 0.0;
+
+    double lr = 0.01;
+
+    nc::NdArray<double> a0 = { { 1, 2 }, { 3, 4 } };
+
+
+    for (int i = 0; i < n_iters; i++) {
+        auto z = nc::dot<double>(X, weights) + bias;
+        auto predictions = sigmoid(z, 150);
+
+        auto dw = 1.0 / n_samples * nc::dot(X.transpose(), (predictions - y));
+        auto db = 1.0 / n_samples * nc::sum<double>(predictions - y);
+
+        // std::cout << weights << std::endl;
+        // std::cout << bias << std::endl;
+        // std::cout << dw << std::endl;
+        // std::cout << db << std::endl;
+        weights = weights - lr * dw;
+        bias = bias - (lr * db)(0, 0); // convert 1 element nc::NdArray to a double by accessing the (0, 0) index
+        // std::cout << weights << std::endl;
+        // std::cout << bias << std::endl;
+
+
+        if (i % 80 == 0) {
+            auto cost = compute_BCE_cost(predictions, y, n_samples);
+            std::cout << "Cost at iteration " << i << ": " << cost << std::endl;
+
+            std::cout << predictions << std::endl;
+        }
+        
+
+    }
+
 
     return 0;
 
